9a869d37bd5ce1f2100b4b23ebf870f0ef6e924d
Melih Keskin <melihkeskinn@gmail.com>
2020-05-29 16:29:40 +0200

    vue-global-filter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-Vue.config.productionTip = false
+// global filter
+Vue.filter("snippet", (val) => {
+  if (!val || typeof val != "string") {
+    return "";
+  }
+  val = val.slice(0,50)
+  return val
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
