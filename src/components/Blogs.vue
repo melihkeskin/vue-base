f1c3334066371959e4e8092b8bd536bffeffe614
Melih Keskin <melihkeskinn@gmail.com>
2020-05-29 16:42:36 +0200

    computed

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h3>{{ blogTitle }}</h3>
-    <div v-for="post in posts" :key="post.id">
+    <input type="text" v-model="searchTerm" />
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
       <p>{{ post.body | snippet }}</p>
     </div>
@@ -21,7 +22,15 @@ export default {
     return {
       blogTitle: "Blogs",
       posts: [],
+      searchTerm: "",
     };
+  },
+  computed: {
+    filteredPosts() {
+      return this.posts.filter((post) => {
+        return post.title.match(this.searchTerm);
+      });
+    },
   },
   created() {
     axios
